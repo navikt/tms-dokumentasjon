84495557d42dbd2bbca76824a061287c92c825f6
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-01-31 15:49:26 +0100

    footer-semantikk

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,10 +3,12 @@ import styles from "./Footer.module.css"
 
 const Footer = () =>
     <div className={styles.footer}>
-        <p id={"slack"}>Min side på slack</p>
-        <p>#team-personbruker</p>
-        <p>#brukernotifikasjoner</p>
-        <p>#utkast</p>
+        <section>
+            <h2 id={"slack"}>Min side på slack</h2>
+            <a href={"https://nav-it.slack.com/archives/C9CJ794PP"}>#team-personbruker</a>
+            <a href={"https://nav-it.slack.com/archives/CR61BPH7G"}>#brukernotifikasjoner</a>
+            <a href={"https://nav-it.slack.com/archives/C04E3K5MMMY"}>#utkast</a>
+        </section>
     </div>
 
 
