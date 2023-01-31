226665f8439394d37a4212e83f47484d576c3a5b
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-01-31 13:40:06 +0100

    Varsler hovedside

--- pages/varsler/index.tsx
+++ pages/varsler/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {NextPage} from 'next'
 import Link from "next/link";
+import styles from "./varsler.module.css"
 
-const Varsel: NextPage = () => {
+const Varsler: NextPage = () => {
 
     return (
-        <main>
-            <ul>
+        <main className={styles.undersider}>
+            <h1>Varsler</h1>
+            <ul >
                 <li><Link href={"varsler/konsumere"}> Konsumere varsel-hendelser</Link></li>
             </ul>
         </main>
@@ -14,4 +16,4 @@ const Varsel: NextPage = () => {
 }
 
 
-export default Varsel
+export default Varsler
