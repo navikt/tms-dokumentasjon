8a6c45177d0b030963e7cc9fda9eec3a8f26ea9c
rannveigskjerve <rannveigskjerve@users.noreply.github.com>
2024-01-11 11:45:25 +0000

    deploy: 1612c58b718aba9f966fcf6051ca33838df47b0b

---
+++ scripts/override_navbar-background.js
@@ -0,0 +1,4 @@
+const navbar = document.getElementsByClassName("bg-primary")[0]
+navbar.classList.remove("bg-primary")
+document.getElementsByClassName("navbar-dark")[0].classList.remove("navbar-dark")
+navbar.classList.add("tms-navbar")
