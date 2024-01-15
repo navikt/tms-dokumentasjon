851baba77dec96a86ed2b8e3efe0fc1002f2f7a1
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2024-01-15 09:24:51 +0100

    legg til flyttamelding

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,21 +6,27 @@ interface NavBarProps {
 }
 
 const Navbar = ({currentLocation}: NavBarProps) =>
-    <nav>
-        <div className={"navbar left"}>
-            <Link href={"/"} className={currentLocation == "" ? "current" : ""}>Min side
-                utvikler-docs </Link>
-        </div>
-        <div className={"navbar right"}>
-            <Link href={"/innholdsguide"}
-                  className={currentLocation == "innholdsguide" ? "current" : ""}>Innholdsguide</Link>
-            <Link href={"/utkast"} className={currentLocation == "utkast" ? "current" : ""}>Utkast</Link>
-            <Link href={"/varsler"}
-                  className={currentLocation == "varsler" ? "current" : ""}>Varsler</Link>
-            <Link href={"/mikrofrontend"}
-                  className={currentLocation == "mikrofrontend" ? "current" : ""}>Mikrofrontend</Link>
-            <Link id="kattenmin" href={"/katt"}>Mjau</Link>
-        </div>
-    </nav>
+    <div>
+        <p>
+            Vi har flytta! Oppdatert dokumentasjon finner du på <a
+            href="https://navikt.github.io/tms-dokumentasjon/"> Github pages </a>
+        </p>
+        <nav>
+            <div className={"navbar left"}>
+                <Link href={"/"} className={currentLocation == "" ? "current" : ""}>Min side
+                    utvikler-docs </Link>
+            </div>
+            <div className={"navbar right"}>
+                <Link href={"/innholdsguide"}
+                      className={currentLocation == "innholdsguide" ? "current" : ""}>Innholdsguide</Link>
+                <Link href={"/utkast"} className={currentLocation == "utkast" ? "current" : ""}>Utkast</Link>
+                <Link href={"/varsler"}
+                      className={currentLocation == "varsler" ? "current" : ""}>Varsler</Link>
+                <Link href={"/mikrofrontend"}
+                      className={currentLocation == "mikrofrontend" ? "current" : ""}>Mikrofrontend</Link>
+                <Link id="kattenmin" href={"/katt"}>Mjau</Link>
+            </div>
+        </nav>
+    </div>
 
 export default Navbar
